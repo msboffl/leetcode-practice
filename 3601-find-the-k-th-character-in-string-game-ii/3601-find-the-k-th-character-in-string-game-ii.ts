1a9f5cd92c67605ce5c8b0f78541649316f783d7
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-07-04 19:35:19 +0530

    Time: 2 ms (54.55%), Space: 56.8 MB (90.91%) - LeetHub

---
+++ 3601-find-the-k-th-character-in-string-game-ii/3601-find-the-k-th-character-in-string-game-ii.ts
@@ -0,0 +1,14 @@
+function kthCharacter(k: number, operations: number[]): string {
+    let ans = 0;
+    while (k !== 1) {
+        let t = Math.floor(Math.log2(k));
+        if (Number(1n << BigInt(t)) === k) {
+            t--;
+        }
+        k -= Number(1n << BigInt(t));
+        if (operations[t]) {
+            ans++;
+        }
+    }
+    return String.fromCharCode("a".charCodeAt(0) + (ans % 26));
+}
