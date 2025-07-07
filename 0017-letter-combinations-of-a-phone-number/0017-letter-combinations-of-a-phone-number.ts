0bd30924eabdc5185cf7ba17321ba1f6c836293a
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-07-07 21:37:11 +0530

    Time: 0 ms (100%), Space: 55.5 MB (43.94%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.ts
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.ts
@@ -1,29 +1,30 @@
 function letterCombinations(digits: string): string[] {
     if(digits.length === 0) return [];
-    const map = {
-        "2": "abc",
-        "3": "def",
-        "4": "ghi",
-        "5": "jkl",
-        "6": "mno",
-        "7": "pqrs",
-        "8": "tuv",
-        "9": "wxyz",
+
+    let map = {
+        2: "abc",
+        3: "def",
+        4: "ghi",
+        5: "jkl",
+        6: "mno",
+        7: "pqrs",
+        8: "tuv",
+        9: "wxyz"
     }
 
-    const res = [];
+    const ans = [];
     function backtrack(index, path) {
         if(path.length === digits.length) {
-            res.push(path);
+            ans.push(path);
             return;
         }
 
         let letters = map[digits[index]];
         for(let letter of letters) {
-            backtrack(index + 1, path + letter)
+            backtrack(index+1, path + letter);
         }
     }
 
     backtrack(0, "");
-    return res;
+    return ans;
 };
