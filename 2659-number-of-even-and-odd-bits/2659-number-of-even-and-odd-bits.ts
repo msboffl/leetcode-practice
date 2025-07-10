c7c30fd53d5d1d28275c2fbae597604a7918d69f
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-07-10 11:16:11 +0530

    Time: 2 ms (50%), Space: 57.8 MB (75%) - LeetHub

--- 2659-number-of-even-and-odd-bits/2659-number-of-even-and-odd-bits.ts
+++ 2659-number-of-even-and-odd-bits/2659-number-of-even-and-odd-bits.ts
@@ -1,20 +1,19 @@
 function evenOddBit(n: number): number[] {
     const binary = n.toString(2);
+
     let even = 0;
     let odd = 0;
-    let index = 0;
 
-    for(let i = binary.length-1; i >= 0; i--) {
-        if(index % 2 === 0 && binary[i] === '1') {
-            even++;
+    binary.split('').reverse().forEach((bit, index) => {
+        if(bit === '1') {
+            if(index % 2 === 1) {
+                odd++
+            } else {
+                even++
+            }
         }
-
-        if(index % 2 === 1 && binary[i] === '1') {
-            odd++;
-        }
-
-        index++;
-    }
+    })
 
     return [even, odd];
+
 };
